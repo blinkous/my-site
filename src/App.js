f62401f9b3b8b14de6390e4366b79e25cd1e9b06
blinkous <saradhani1@gmail.com>
2019-06-30 21:12:35 -0400

    added the diamonds

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './css/App.css';
+import Diamonds from './components/Diamonds';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <React.Fragment>
+        <Diamonds id="diamonds-left"/>
+        <Diamonds id="diamonds-right"/>
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
