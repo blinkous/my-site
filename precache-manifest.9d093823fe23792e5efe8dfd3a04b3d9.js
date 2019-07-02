466b185fa4440d626630a5154c1934b62ae1a785
blinkous <saradhani1@gmail.com>
2019-07-02 19:22:39 -0400

    Updates

---
+++ precache-manifest.9d093823fe23792e5efe8dfd3a04b3d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf02157a0b4b6311479b1a29b82bc43",
+    "url": "/blinkous/my-site/index.html"
+  },
+  {
+    "revision": "4e6698e7d4129ace3bde",
+    "url": "/blinkous/my-site/static/css/main.8d83e693.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/blinkous/my-site/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "4e6698e7d4129ace3bde",
+    "url": "/blinkous/my-site/static/js/main.5dc8b782.chunk.js"
+  },
+  {
+    "revision": "8a181f24da8298efa03a",
+    "url": "/blinkous/my-site/static/js/runtime~main.1a8ec44f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/blinkous/my-site/precache-manifest.9d093823fe23792e5efe8dfd3a04b3d9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blinkous/my-site/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
