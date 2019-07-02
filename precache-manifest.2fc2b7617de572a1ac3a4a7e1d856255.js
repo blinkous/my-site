1782e8c20b89b9184494aac5979da47e1cf793ee
blinkous <saradhani1@gmail.com>
2019-07-02 19:39:01 -0400

    Updates

--- precache-manifest.2fc2b7617de572a1ac3a4a7e1d856255.js
+++ precache-manifest.2fc2b7617de572a1ac3a4a7e1d856255.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f810e8bb03d3e428c209a9ec476f890f",
+    "revision": "25122dc8c0c1010e7046d079a94784c5",
     "url": "/my-site/index.html"
   },
   {
-    "revision": "16c66acb3e53cf4cb703",
-    "url": "/my-site/static/css/main.8735abb4.chunk.css"
+    "revision": "7c94525938e918d3a252",
+    "url": "/my-site/static/css/main.8d83e693.chunk.css"
   },
   {
     "revision": "9eee67cf782a38d35aeb",
     "url": "/my-site/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "16c66acb3e53cf4cb703",
+    "revision": "7c94525938e918d3a252",
     "url": "/my-site/static/js/main.244db8e4.chunk.js"
   },
   {
